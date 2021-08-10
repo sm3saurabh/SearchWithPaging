8c93f1f4f14d0456827743c3f26f1bd1ba9e5c18
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-08-10 18:51:22 +0530

    add: support for page number based pagination using remote mediator

--- app/src/main/java/dev/saurabhmishra/searchwithpagination/mediator/SearchRemoteMediator.kt
+++ app/src/main/java/dev/saurabhmishra/searchwithpagination/mediator/SearchRemoteMediator.kt
@@ -7,22 +7,62 @@ import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
 import dev.saurabhmishra.searchwithpagination.repo.SearchRepo
 import dev.saurabhmishra.searchwithpagination.sources.local.entities.PhotoEntity
+import dev.saurabhmishra.searchwithpagination.sources.network.helper.SafeResult
+import dev.saurabhmishra.searchwithpagination.utils.AppConstants
 import dev.saurabhmishra.searchwithpagination.utils.SearchQueryPublisher
 
 
 @OptIn(ExperimentalPagingApi::class)
 class SearchRemoteMediator(
-  private val searchRepo: SearchRepo
-): RemoteMediator<Int, PhotoEntity>() {
+    private val searchRepo: SearchRepo
+) : RemoteMediator<Int, PhotoEntity>() {
 
-  override suspend fun load(
-    loadType: LoadType,
-    state: PagingState<Int, PhotoEntity>
-  ): MediatorResult {
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, PhotoEntity>
+    ): MediatorResult {
+
+        val entity = when (loadType) {
+            LoadType.APPEND -> {
+                state.lastItemOrNull()
+            }
+            LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
+            LoadType.REFRESH -> null
+        }
+
+        val pageNumber = getPageNumberForCurrentEntity(entity)
+        return loadAndSavePhotos(pageNumber = pageNumber)
+    }
+
+    private suspend fun getPageNumberForCurrentEntity(entity: PhotoEntity?): Int {
+
+        entity ?: return AppConstants.INITIAL_PAGE
+
+        return searchRepo.getPageNumberByPhotoId(entity.id)
+    }
+
+    private suspend fun loadAndSavePhotos(pageNumber: Int): MediatorResult {
+        val currentQuery = SearchQueryPublisher.getCurrentQuery()
+
+        return when (val photosResult = searchRepo.loadAndSavePhotos(currentQuery, pageNumber)) {
+            is SafeResult.Success -> {
+                val isReloadSuccess = photosResult.data.success
+
+                if (isReloadSuccess) {
+                    MediatorResult.Success(endOfPaginationReached = photosResult.data.endReached)
+                } else {
+                    MediatorResult.Error(SearchRefreshFailedException())
+                }
 
 
-    val currentQuery = SearchQueryPublisher.getCurrentQuery()
-    searchRepo.loadAndSavePhotos(currentQuery, pageNumber = )
-  }
+            }
+            is SafeResult.Failure -> {
+                MediatorResult.Error(photosResult.exception)
+            }
+        }
+
+    }
 
 }
+
+class SearchRefreshFailedException: Exception(message = "Search failed in either loading or saving in database")
--- app/src/main/java/dev/saurabhmishra/searchwithpagination/repo/SearchRepo.kt
+++ app/src/main/java/dev/saurabhmishra/searchwithpagination/repo/SearchRepo.kt
@@ -3,20 +3,27 @@ package dev.saurabhmishra.searchwithpagination.repo
 import dev.saurabhmishra.searchwithpagination.mappers.toPhotoEntity
 import dev.saurabhmishra.searchwithpagination.sources.local.SearchLocalSource
 import dev.saurabhmishra.searchwithpagination.sources.local.entities.PhotoEntity
+import dev.saurabhmishra.searchwithpagination.sources.local.entities.PhotoPageMetadata
 import dev.saurabhmishra.searchwithpagination.sources.network.SearchNetworkSource
 import dev.saurabhmishra.searchwithpagination.sources.network.helper.SafeResult
-import dev.saurabhmishra.searchwithpagination.sources.network.models.PhotoResponse
 import dev.saurabhmishra.searchwithpagination.sources.network.models.PhotosSearchResponse
+import dev.saurabhmishra.searchwithpagination.utils.AppConstants
 import dev.saurabhmishra.searchwithpagination.utils.Logger
 import kotlinx.coroutines.flow.Flow
 import java.lang.Exception
 
 
+data class SearchLoadResult(
+    val success: Boolean,
+    val endReached: Boolean
+)
+
 interface SearchRepo {
-    suspend fun loadAndSavePhotos(searchQuery: String, pageNumber: Int): SafeResult<Boolean>
+    suspend fun loadAndSavePhotos(searchQuery: String, pageNumber: Int): SafeResult<SearchLoadResult>
     suspend fun deleteEverything()
     fun getFavoritePhotos(): Flow<PhotoEntity>
     fun getPhotosForSearchQuery(query: String): Flow<PhotoEntity>
+    suspend fun getPageNumberByPhotoId(id: String): Int
 }
 
 class SearchRepoImpl(
@@ -27,11 +34,11 @@ class SearchRepoImpl(
     override suspend fun loadAndSavePhotos(
         searchQuery: String,
         pageNumber: Int
-    ): SafeResult<Boolean> {
+    ): SafeResult<SearchLoadResult> {
 
         return when (val photosResult =
             searchNetworkSource.searchPhotosForTag(searchQuery, pageNumber)) {
-            is SafeResult.Success -> handlePhotoLoadSuccess(searchQuery, photosResult)
+            is SafeResult.Success -> handlePhotoLoadSuccess(searchQuery, photosResult, pageNumber)
             is SafeResult.Failure -> photosResult
         }
     }
@@ -48,18 +55,32 @@ class SearchRepoImpl(
         return searchLocalSource.getPhotosForSearchQuery(query)
     }
 
+    override suspend fun getPageNumberByPhotoId(id: String): Int {
+        return searchLocalSource.getPhotoPageByPhotoId(id)
+    }
+
     private suspend fun handlePhotoLoadSuccess(
         searchQuery: String,
-        photosResult: SafeResult.Success<PhotosSearchResponse>
-    ): SafeResult<Boolean> {
+        photosResult: SafeResult.Success<PhotosSearchResponse>,
+        pageNumber: Int
+    ): SafeResult<SearchLoadResult> {
 
         val photos = photosResult.data.photos.photo
 
-        val photoEntities = photos.map { photo -> photo.toPhotoEntity(searchQuery = searchQuery) }
+        val photoEntities = photos.map { photo ->
+            photo.toPhotoEntity(searchQuery = searchQuery)
+        }
+
+        val photosMetaData = photos.map { photo ->
+            PhotoPageMetadata(photo.id, pageNumber)
+        }
+
+        val endReached = photos.size < AppConstants.OBJECTS_PER_PAGE
 
         return try {
-            val success = searchLocalSource.savePhotos(photoEntities)
-            SafeResult.Success(success)
+            val success = searchLocalSource.savePhotos(photoEntities)  &&
+                searchLocalSource.savePhotoMetaData(photosMetaData)
+            SafeResult.Success(SearchLoadResult(success = success, endReached = endReached))
         } catch (ex: Exception) {
             Logger.error("Exception while saving photos in db", ex)
             SafeResult.Failure(ex)
